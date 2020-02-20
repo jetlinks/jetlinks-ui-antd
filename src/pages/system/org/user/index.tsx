b8909672e398ccb74226525cdac3e14e9116a2ae
Lind <448929489@qq.com>
2020-02-20 15:44:11 +0800

    优化批量解绑

--- src/pages/system/org/user/index.tsx
+++ src/pages/system/org/user/index.tsx
@@ -1,4 +1,4 @@
-import { Drawer, List, Avatar, Popconfirm, message, Button } from 'antd';
+import { Drawer, Popconfirm, message, Button, Table } from 'antd';
 import React, { useEffect, useState } from 'react';
 import apis from '@/services';
 import encodeQueryParam from '@/utils/encodeParam';
@@ -11,14 +11,20 @@ interface Props {
 interface State {
   userList: any[];
   bindVisible: boolean;
+  selectRow: any[];
+  loading: boolean;
 }
 const BindUser: React.FC<Props> = props => {
   const initState: State = {
     userList: [],
     bindVisible: false,
+    selectRow: [],
+    loading: false,
   };
   const [userList, setUserList] = useState(initState.userList);
   const [bindVisible, setBindVisible] = useState(initState.bindVisible);
+  const [selectRow, setSelectRow] = useState(initState.selectRow);
+  const [loading, setLoading] = useState(initState.loading);
 
   const handleSearch = () => {
     apis.org.bindUser(encodeQueryParam({ terms: { dimensionId: props.data.id } })).then(res => {
@@ -39,42 +45,119 @@ const BindUser: React.FC<Props> = props => {
     apis.org.unBindUser(item.id).then(repsonse => {
       if (repsonse) {
         message.success('解绑成功');
+        const temp = selectRow.filter(i => i !== item.id);
+        setSelectRow(temp);
       }
       handleSearch();
     });
   };
 
+  const batchRemove = () => {
+    setLoading(true);
+    let count = 0;
+    selectRow.forEach(i => {
+      apis.org
+        .unBindUser(i.id)
+        .then(response => {
+          if (response) {
+            count += 1;
+            if (count === selectRow.length) {
+              message.error('解绑成功');
+              setLoading(false);
+              setSelectRow([]);
+              handleSearch();
+            }
+          }
+        })
+        .catch(() => {
+          message.error('解绑失败');
+          setLoading(false);
+          handleSearch();
+        });
+    });
+  };
+
+  const rowSelection = {
+    onChange: (selectedRowKeys: any, selectedRows: any) => {
+      // setSelectRowKeys();
+      setSelectRow(selectedRows);
+      // console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+    },
+    getCheckboxProps: (record: any) => ({
+      disabled: record.name === 'Disabled User', // Column configuration not to be checked
+      name: record.name,
+    }),
+  };
+
+  console.log(userList, 'list');
+
   return (
     <Drawer visible title="绑定用户" width="40VW" onClose={() => props.close()}>
-      <Button type="primary" icon="plus" onClick={() => setBindVisible(true)}>
+      <Button
+        style={{ marginBottom: 10 }}
+        type="primary"
+        icon="plus"
+        onClick={() => setBindVisible(true)}
+      >
         新建
       </Button>
-      <List
-        className="demo-loadmore-list"
-        itemLayout="horizontal"
+      {selectRow.length > 0 && (
+        <Button
+          onClick={() => {
+            batchRemove();
+          }}
+          type="danger"
+          style={{ marginLeft: 10 }}
+        >
+          {`解除绑定：${selectRow.length}项`}
+        </Button>
+      )}
+      <Table
+        loading={loading}
+        rowKey="id"
+        rowSelection={rowSelection}
+        columns={[
+          {
+            dataIndex: 'userName',
+            title: '用户名',
+          },
+          {
+            title: '操作',
+            render: (record: any) => (
+              <Popconfirm title="确认删除绑定关系吗？" onConfirm={() => remove(record)}>
+                <a>删除</a>
+              </Popconfirm>
+            ),
+          },
+        ]}
         dataSource={userList}
-        renderItem={item => (
-          <List.Item
-            actions={[
-              <Popconfirm
-                title="确定删除该绑定关系？"
-                onConfirm={() => {
-                  remove(item);
-                }}
-              >
-                <a key="list-loadmore-edit">删除</a>
-              </Popconfirm>,
-            ]}
-          >
-            <List.Item.Meta
-              avatar={
-                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-              }
-              title={<a>{item.userName}</a>}
-            />
-          </List.Item>
-        )}
       />
+      {/* <List
+                className="demo-loadmore-list"
+                itemLayout="horizontal"
+                dataSource={userList}
+                renderItem={item => (
+                    <List.Item
+                        actions={[
+                            <Popconfirm
+                                title="确定删除该绑定关系？"
+                                onConfirm={() => {
+                                    remove(item);
+                                }}
+                            >
+                                <a key="list-loadmore-edit">删除</a>
+                            </Popconfirm>,
+                        ]}
+                    >
+                        <List.Item.Meta
+                            avatar={
+                                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                            }
+                            title={<a>{item.userName}</a>}
+                        />
+                    </List.Item>
+                )}
+            /> */}
       {bindVisible && (
         <UserList
           data={props.data}
