be01acf904d5a20a32e28a4e1199a474a4751045
Lind <448929489@qq.com>
2020-02-24 15:19:08 +0800

    规则引擎：获取当前支持的节点。只显示支持的节点。

--- src/pages/rule-engine/flow/components/EditorItemPanel/RuleItemPanel.tsx
+++ src/pages/rule-engine/flow/components/EditorItemPanel/RuleItemPanel.tsx
@@ -1,73 +1,66 @@
 import { ItemPanel, Item } from 'gg-editor';
 
-import { Collapse, Card } from 'antd';
-import React, { Component } from 'react';
+import { Collapse } from 'antd';
+import React, { useState, useEffect } from 'react';
 import styles from './index.less';
 import items from './item';
-import { ItemType } from './RuleItem';
+import apis from '@/services';
+
 const { Panel } = Collapse;
 
-interface RuleItemPanelProps {
+interface Props {}
 
+interface State {
+  supportNode: string[];
 }
 
-interface RuleItemPanelState {
-    itemData: ItemType[];
-}
+const RuleItemPanel: React.FC<Props> = () => {
+  const initState: State = {
+    supportNode: [],
+  };
 
-class RuleItemPanel extends Component<RuleItemPanelProps, RuleItemPanelState>{
+  const [supportNode, setSupportNode] = useState(initState.supportNode);
 
-    state: RuleItemPanelState = {
-        itemData: [],
-    }
+  useEffect(() => {
+    apis.ruleEngine.nodeList().then(response => {
+      if (response) {
+        setSupportNode(response.result);
+      }
+    });
+  }, []);
 
-    componentDidMount() {
-        this.setState({
-            itemData: items,
-        })
-        const me = this;
-        // this.state.data = { "nodes": [{ "type": "node", "size": "72*72", "shape": "flow-circle", "color": "#FA8C16", "label": "启动任务", "executor": "timer", "x": 423.84375, "y": 167, "id": "67a26f33", "remark": "测试Timer", "config": { "cron": "0 1-2 * * * ? ", "defaultData": "dsfsfs" }, "index": 0 }, { "type": "node", "size": "80*48", "shape": "flow-capsule", "color": "#B37FEB", "label": "Querys", "executor": "sql", "x": 419.84375, "y": 296, "id": "c289e872", "remark": "测试查询sdf", "config": { "dataSourcedId": "mysql", "stream": true, "transaction": true, "script": "select * from table;" }, "index": 1 }, { "type": "node", "size": "120*48", "shape": "flow-rect", "color": "#1890FF", "label": "数据转换", "executor": "dataMapping", "x": 576.84375, "y": 353, "id": "cdd8828a", "index": 3, "remark": "sdfs", "config": { "mappings": [{ "key": "0", "source": "12312", "target": "31231", "type": "132" }], "keepSourceData": true } }], "edges": [{ "source": "67a26f33", "sourceAnchor": 2, "target": "c289e872", "targetAnchor": 0, "id": "9e1baa72", "index": 2 }, { "source": "c289e872", "sourceAnchor": 1, "target": "cdd8828a", "targetAnchor": 0, "id": "501d68ca", "index": 4 }] };
-        // if (window.onRuleEditorInited) {
-        //     window.onRuleEditorInited({
-        //         setItemData: function (data: any) {
-        //             me.state.itemData = data;
-        //         },
-        //     });
-        // }
-    }
+  const renderItems = () =>
+    items.map(
+      type =>
+        type.items.length > 0 && (
+          <Panel header={type.title} key={type.key}>
+            <div style={{ textAlign: 'center' }}>
+              {type.items.length > 0 &&
+                type.items.map(
+                  item =>
+                    supportNode.findIndex(i => i === item.model.executor) > -1 && (
+                      <Item
+                        type={item.type}
+                        size={item.size}
+                        shape={item.shape}
+                        model={item.model}
+                        src={item.src}
+                        key={item.model.executor}
+                      />
+                    ),
+                )}
+            </div>
+          </Panel>
+        ),
+    );
 
-    renderItems = () => {
-        const { itemData } = this.state;
-
-        return itemData.map(type => (
-            <Panel header={type.title} key={type.key}>
-                <div style={{ textAlign: 'center' }}>
-                    {type.items.length > 0 ? type.items.map(item => (
-                        <Item
-                            type={item.type}
-                            size={item.size}
-                            shape={item.shape}
-                            model={item.model}
-                            src={item.src}
-                            key={item.id}
-                        />
-                    )) : ""}
-                </div>
-            </Panel>
-        ))
-    }
-
-    render() {
-        return (
-            <ItemPanel className={styles.itemPanel}>
-                <Collapse bordered={false} defaultActiveKey={['start']}>
-                    {
-                        this.renderItems()
-                    }
-                </Collapse>
-            </ItemPanel>
-        );
-    }
-}
+  return (
+    <ItemPanel className={styles.itemPanel}>
+      <Collapse bordered={false} defaultActiveKey={['start']}>
+        {renderItems()}
+      </Collapse>
+    </ItemPanel>
+  );
+};
 
 export default RuleItemPanel;
--- src/pages/rule-engine/service.ts
+++ src/pages/rule-engine/service.ts
@@ -1,40 +1,46 @@
-import request from "@/utils/request";
+import request from '@/utils/request';
+
+export async function nodeList() {
+  return request(`/jetlinks/rule-engine/model/executors`, {
+    method: 'GET',
+  });
+}
 
 export async function debug(params: any) {
-    return request('/jetlinks/rule-engine/debug', {
-        method: 'POST',
-        data: params,
-    });
+  return request('/jetlinks/rule-engine/debug', {
+    method: 'POST',
+    data: params,
+  });
 }
 
 export async function debugNode(id: string, params: any) {
-    return request(`/jetlinks/rule-engine/debug/${id}`, {
-        method: 'POST',
-        data: params,
-    });
+  return request(`/jetlinks/rule-engine/debug/${id}`, {
+    method: 'POST',
+    data: params,
+  });
 }
 
 export async function debugResult(id: string, result: string, params: any) {
-    return request(`/jetlinks/rule-engine/debug/${id}/${result}`, {
-        method: 'POST',
-        data: params,
-    });
+  return request(`/jetlinks/rule-engine/debug/${id}/${result}`, {
+    method: 'POST',
+    data: params,
+  });
 }
 
 export async function closeDebug(id: string) {
-    return request(`/jetlinks/rule-engine/debug/${id}`, {
-        method: 'DELETE',
-    });
+  return request(`/jetlinks/rule-engine/debug/${id}`, {
+    method: 'DELETE',
+  });
 }
 
 export async function debugContext(id: string) {
-    return request(`/jetlinks/rule-engine/debug/${id}/contexts`, {
-        method: 'GET',
-    });
+  return request(`/jetlinks/rule-engine/debug/${id}/contexts`, {
+    method: 'GET',
+  });
 }
 
 export async function stopNodeDebug(id: string, nodeId: string) {
-    return request(`/jetlinks/rule-engine/debug/${id}/${nodeId}`, {
-        method: 'DELETE',
-    });
+  return request(`/jetlinks/rule-engine/debug/${id}/${nodeId}`, {
+    method: 'DELETE',
+  });
 }
