dccef59db45d28333ece7921e85e9f43bd44a538
gaoyf <admin@gaiys.com>
2020-03-03 22:00:30 +0800

    网络组件-TCP数据格式优化

--- src/pages/network/type/save/index.tsx
+++ src/pages/network/type/save/index.tsx
@@ -63,8 +63,8 @@ const Save: React.FC<Props> = props => {
         return (
           <>
             <Form.Item label="脚本语言">
-              {getFieldDecorator('parserConfiguration.lang', {
-                initialValue: props.data?.parserConfiguration?.lang,
+              {getFieldDecorator('configuration.parserConfiguration.lang', {
+                initialValue: props.data?.configuration.parserConfiguration?.lang,
               })(
                 <Radio.Group buttonStyle="solid">
                   <Radio.Button value="javascript">JavaScript</Radio.Button>
@@ -73,8 +73,8 @@ const Save: React.FC<Props> = props => {
               )}
             </Form.Item>
             <Form.Item label="解析脚本">
-              {getFieldDecorator('parserConfiguration.script', {
-                initialValue: props.data?.parserConfiguration?.script,
+              {getFieldDecorator('configuration.parserConfiguration.script', {
+                initialValue: props.data?.configuration.parserConfiguration?.script,
               })(<Input.TextArea rows={3} />)}
             </Form.Item>
           </>
