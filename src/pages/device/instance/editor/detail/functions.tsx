fafc34a02d57243a66ac22e37c1bde36bf0def07
gaoyf <admin@gaiys.com>
2020-03-23 11:09:30 +0800

    代码优化

--- src/pages/device/instance/editor/detail/functions.tsx
+++ src/pages/device/instance/editor/detail/functions.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Card, Divider, Form, Input, Select } from 'antd';
+import { Button, Card, Divider, Form, Input, Select, Spin } from 'antd';
 import apis from '@/services';
 
 interface Props {
@@ -13,6 +13,7 @@ interface State {
   debugData: string;
   logs: string;
   functionId: string;
+  spinning:boolean;
 }
 
 const Functions: React.FC<Props> = (props) => {
@@ -24,6 +25,7 @@ const Functions: React.FC<Props> = (props) => {
     debugData: '',
     logs: '',
     functionId: '',
+    spinning:false,
   };
 
   const [functionsSelectList] = useState(initState.functionsSelectList);
@@ -31,15 +33,16 @@ const Functions: React.FC<Props> = (props) => {
   const [debugData, setDebugData] = useState(initState.debugData);
   const [logs, setLogs] = useState(initState.logs);
   const [functionId, setFunctionId] = useState(initState.functionId);
+  const [spinning, setSpinning] = useState(initState.spinning);
 
   useEffect(() => {
     apis.deviceInstance.runInfo(props.device.id)
       .then(response => {
         const tempResult = response?.result;
         if (response.status === 200) {
-          const {functions} = JSON.parse(tempResult.metadata);
+          const { functions } = JSON.parse(tempResult.metadata);
           const map = {};
-          functions.forEach((item:any) => {
+          functions.forEach((item: any) => {
             map[item.id] = item;
             functionsSelectList.push(<Select.Option key={item.id}>{item.name}</Select.Option>);
           });
@@ -51,6 +54,7 @@ const Functions: React.FC<Props> = (props) => {
   }, []);
 
   const debugFunction = () => {
+    setSpinning(true);
     apis.deviceInstance
       .invokedFunction(props.device.id, functionId, JSON.parse(debugData))
       .then(response => {
@@ -58,57 +62,60 @@ const Functions: React.FC<Props> = (props) => {
         if (response.status === 200) {
           setLogs(tempResult);
         }
+        setSpinning(false);
       }).catch(() => {
-        setLogs(`调试错误`);
+      setLogs(`调试错误`);
     });
   };
 
   return (
     <div>
-      <Card style={{ marginBottom: 20 }} title="功能调试">
-        <Form labelCol={{ span: 1 }} wrapperCol={{ span: 23 }}>
-          <Form.Item label="设备功能">
-            <Select placeholder="请选择设备功能" onChange={(e:any) => {
-              setFunctionId(e);
-              const map = {};
-              functionsInfo[e].inputs.forEach((item:any)=>{
-                map[item.id] = item.name;
-              });
-              setDebugData(JSON.stringify(map,null,2))
-            }}>
-              {functionsSelectList}
-            </Select>
-          </Form.Item>
-          <Form.Item label="参数：">
-            <Input.TextArea
-              rows={4} style={{ height: '210px' }}
-              value={debugData}
-              onChange={e => {
-                setDebugData(e.target.value);
-              }}
-              placeholder="参数必须JSON格式"
-            />
-          </Form.Item>
-          <div style={{ textAlign: 'right' }}>
-            <Button
-              type="primary"
-              onClick={() => {
-                debugFunction();
-              }}
-            >
-              执行
-            </Button>
-            <Divider type="vertical"/>
-            <Button type="ghost" onClick={() => setLogs('')}>
-              清空
-            </Button>
-          </div>
+      <Spin tip="操作中..." spinning={spinning}>
+        <Card style={{ marginBottom: 20 }} title="功能调试">
+          <Form labelCol={{ span: 1 }} wrapperCol={{ span: 23 }}>
+            <Form.Item label="设备功能">
+              <Select placeholder="请选择设备功能" onChange={(e: any) => {
+                setFunctionId(e);
+                const map = {};
+                functionsInfo[e].inputs.forEach((item: any) => {
+                  map[item.id] = item.name;
+                });
+                setDebugData(JSON.stringify(map, null, 2));
+              }}>
+                {functionsSelectList}
+              </Select>
+            </Form.Item>
+            <Form.Item label="参数：">
+              <Input.TextArea
+                rows={4} style={{ height: '210px' }}
+                value={debugData}
+                onChange={e => {
+                  setDebugData(e.target.value);
+                }}
+                placeholder="参数必须JSON格式"
+              />
+            </Form.Item>
+            <div style={{ textAlign: 'right' }}>
+              <Button
+                type="primary"
+                onClick={() => {
+                  debugFunction();
+                }}
+              >
+                执行
+              </Button>
+              <Divider type="vertical"/>
+              <Button type="ghost" onClick={() => setLogs('')}>
+                清空
+              </Button>
+            </div>
 
-          <Form.Item label="调试结果：" style={{paddingTop:20}}>
-            <Input.TextArea rows={4} value={logs}/>
-          </Form.Item>
-        </Form>
-      </Card>
+            <Form.Item label="调试结果：" style={{ paddingTop: 20 }}>
+              <Input.TextArea rows={4} value={logs}/>
+            </Form.Item>
+          </Form>
+        </Card>
+      </Spin>
     </div>
   );
 };
