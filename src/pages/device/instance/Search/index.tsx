2f97cf0273bf80d88700859a4a54299a374951bd
gaoyf <admin@gaiys.com>
2020-03-24 18:10:53 +0800

    设备实例-设备名称模糊查询改为精准查询

--- src/pages/device/instance/Search/index.tsx
+++ src/pages/device/instance/Search/index.tsx
@@ -57,7 +57,7 @@ const Search: React.FC<Props> = props => {
   const simpleItems: any[] = [
     {
       label: '设备名称',
-      key: 'name$LIKE',
+      key: 'name$link',
       component: <Input placeholder="请输入" />,
     },
     {
