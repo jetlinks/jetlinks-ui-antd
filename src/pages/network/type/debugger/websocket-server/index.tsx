3b4fe0845bc4968d5f443d95406399c97734e240
lind <448929489@qq.com>
2020-05-25 17:35:35 +0800

    #52 websocket server调试功能

--- src/pages/network/type/debugger/websocket-server/index.tsx
+++ src/pages/network/type/debugger/websocket-server/index.tsx
@@ -1,9 +1,7 @@
-import { Modal, Button, Divider, Form, Select } from 'antd';
+import { Modal, Button, Divider, Form, Input } from 'antd';
 
-import React, { Fragment, useState } from 'react';
-import { getAccessToken } from '@/utils/authority';
-import { wrapAPI } from '@/utils/utils';
-import { EventSourcePolyfill } from 'event-source-polyfill';
+import React, { Fragment, useState, useEffect } from 'react';
+import { getWebsocket } from '@/layouts/GlobalWebSocket';
 
 interface Props {
   close: Function;
@@ -16,36 +14,54 @@ interface State {
 const WebSocketServer: React.FC<Props> = props => {
   const { item } = props;
   const initState: State = {
-    type: 'JSON',
+    type: 'HEX',
     logs: [],
   };
-  const [type, setType] = useState(initState.type);
   const [logs, setLogs] = useState(initState.logs);
-  const [sourceState, setSourceState] = useState<any>();
+  const [subs, setSubs] = useState<any>();
+  const [responseData, setResponseData] = useState<any>();
+
+  const closeEventSource = () => {
+    if (subs) {
+      subs.unsubscribe();
+    }
+  };
+
+  useEffect(
+    () => () => {
+      closeEventSource();
+    },
+    [],
+  );
 
   const debug = () => {
     logs.push('开始调试');
     setLogs([...logs]);
-    const eventSource = new EventSourcePolyfill(
-      wrapAPI(
-        `/jetlinks/network/websocket/server/${
-        item.id
-        }/_subscribe/${type}?:X_Access_Token=${getAccessToken()}`,
-      ),
+
+    if (subs) {
+      subs.unsubscribe()
+    }
+    // /network/websocket/server/{networkId}/_subscribe
+    const ws = getWebsocket(
+      `websocket-server-debug`,
+      `/network/websocket/server/${item.id}/_subscribe`,
+      {
+        response: responseData
+      }
+    ).subscribe(
+      (resp: any) => {
+        const { payload } = resp;
+        setLogs(l => [...l, JSON.stringify(payload)]);
+      }
     );
-    setSourceState(eventSource);
-    eventSource.onerror = () => {
-      setLogs([...logs, '断开连接']);
-    };
-    eventSource.onmessage = e => {
-      // 追加日志
-      setLogs(l => [...l, e.data]);
-    };
-    eventSource.onopen = () => {
-      // setLogs(`${logs}开启推送`);
-      // message.error('关闭链接');
-    };
+    setSubs(ws);
   };
+
+  const clearLog = () => {
+    logs.splice(0, logs.length);
+    setLogs([]);
+  };
+
   return (
     <Modal
       visible
@@ -68,7 +84,7 @@ const WebSocketServer: React.FC<Props> = props => {
             onClick={() => {
               logs.push('结束调试');
               setLogs([...logs]);
-              if (sourceState) sourceState.close();
+              if (subs) subs.unsubscribe();
             }}
           >
             结束
@@ -76,10 +92,7 @@ const WebSocketServer: React.FC<Props> = props => {
           <Divider type="vertical" />
           <Button
             type="ghost"
-            onClick={() => {
-              logs.splice(0, logs.length);
-              setLogs([]);
-            }}
+            onClick={clearLog}
           >
             清空
           </Button>
@@ -87,7 +100,7 @@ const WebSocketServer: React.FC<Props> = props => {
       }
     >
       <Form labelCol={{ span: 4 }} wrapperCol={{ span: 20 }}>
-        <Form.Item label="数据类型">
+        {/* <Form.Item label="数据类型">
           <Select
             defaultValue={type}
             onChange={(e: string) => {
@@ -99,6 +112,18 @@ const WebSocketServer: React.FC<Props> = props => {
             <Select.Option value="STRING">字符串</Select.Option>
             <Select.Option value="HEX">16进制</Select.Option>
           </Select>
+        </Form.Item> */}
+
+        <Form.Item label="响应数据">
+          <Input.TextArea
+            rows={8}
+            placeholder="16进制请使用0x开头"
+            onChange={e => {
+              setResponseData(e.target.value);
+              localStorage.setItem('websocket-server-debug', e.target.value);
+            }}
+            value={responseData}
+          />
         </Form.Item>
         <Divider>调试日志</Divider>
         <div style={{ height: 350, overflow: 'auto' }}>
