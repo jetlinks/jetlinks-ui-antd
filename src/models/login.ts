ddf9cca189975d0bfd995b431906a7853fe61d41
Lind <448929489@qq.com>
2020-07-30 16:30:15 +0800

    fix(多租户): 用户登录判断租户身份
    修复没有租户信息的用户无法登录

--- src/models/login.ts
+++ src/models/login.ts
@@ -45,7 +45,10 @@ const Model: LoginModelType = {
       if (response.status === 200) {
         setAccessToken(response.result.token);
         setAutz(response.result);
-        localStorage.setItem('tenants-admin', response.result?.user?.tenants[0]?.adminMember);
+        const tenants = response.result?.user?.tenants;
+        if (tenants && tenants[0]) {
+          localStorage.setItem('tenants-admin', response.result?.user?.tenants[0]?.adminMember);
+        }
         reloadAuthorized();
         const version = yield call(systemVersion);
         if (version) {
