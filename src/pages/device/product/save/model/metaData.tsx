7a6923efdd10bed7ef99ef649a8b09a602666e15
gao <admin@gaiys.com>
2020-08-04 18:31:02 +0800

    优化导出物模型界面提示语

--- src/pages/device/product/save/model/metaData.tsx
+++ src/pages/device/product/save/model/metaData.tsx
@@ -43,7 +43,7 @@ const MetaData: React.FC<Props> = props => {
       <div style={{background: 'rgb(236, 237, 238)'}}>
         <p style={{padding: 10}}>
           物模型是对设备在云端的功能描述，包括设备的属性、服务和事件。物联网平台通过定义一种物的描述语言来描述物模型，称之为 TSL（即 Thing Specification Language），采用 JSON 格式，您可以根据
-          TSL 组装上报设备的数据。您可以导出完整物模型，用于云端应用开发；您也可以只导出精简物模型，配合设备端 SDK 实现设备开发。
+          TSL 组装上报设备的数据。您可以导出完整物模型，用于云端应用开发。
         </p>
       </div>
       <div style={{border: '1px solid #E9E9E9', marginTop: 20}}>
