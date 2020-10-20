c70dbccf3b555ff58a542636d1611f7c9dc10207
sun-chaochao <2642441182@qq.com>
2020-10-20 16:21:52 +0800

    登录界面增加OAuth2支持

--- src/models/login.ts
+++ src/models/login.ts
@@ -27,6 +27,20 @@ export interface LoginModelType {
   };
 }
 
+let getQueryString =(url: string) => {
+  if (url) {
+    url = url.substr(url.indexOf("?") + 1); //字符串截取，比我之前的split()方法效率高
+  }
+  var result = {}, //创建一个对象，用于存name，和value
+    queryString = url || window.location.hash.substring(1),
+    re = /([^&=]+)=([^&]*)/g, //正则，具体不会用
+    m;
+  while (m = re.exec(queryString)) { //exec()正则表达式的匹配，具体不会用
+    result[decodeURIComponent(m[1])] = decodeURIComponent(m[2]); //使用 decodeURIComponent() 对编码后的 URI 进行解码
+  }
+  return result;
+}
+
 const Model: LoginModelType = {
   namespace: 'login',
 
@@ -46,35 +60,44 @@ const Model: LoginModelType = {
         setAccessToken(response.result.token);
         setAutz(response.result);
         const tenants = response.result?.user?.tenants;
-        if (tenants && tenants[0]) {
-          localStorage.removeItem('tenants-admin');
-          //找到主租户
-          const temp = (response.result?.user?.tenants || []).find((i: any) => i.mainTenant).adminMember;
-          localStorage.setItem('tenants-admin', temp);
-        }
-        reloadAuthorized();
-        const version = yield call(systemVersion);
-        if (version) {
-          localStorage.setItem('system-version', version?.result?.edition);
-        }
-        const urlParams = new URL(window.location.href);
-        const params = getPageQuery();
-        let { redirect } = params as { redirect: string };
-        if (redirect) {
-          const redirectUrlParams = new URL(redirect);
-          if (redirectUrlParams.origin === urlParams.origin) {
-            redirect = redirect.substr(urlParams.origin.length);
-            if (redirect.match(/^\/.*#/)) {
-              redirect = redirect.substr(redirect.indexOf('#') + 1);
-            }
-          } else {
-            window.location.href = '/';
-            return;
+          if (tenants && tenants[0]) {
+            localStorage.removeItem('tenants-admin');
+            //找到主租户
+            const temp = (response.result?.user?.tenants || []).find((i: any) => i.mainTenant).adminMember;
+            localStorage.setItem('tenants-admin', temp);
           }
+          reloadAuthorized();
+          const version = yield call(systemVersion);
+          if (version) {
+            localStorage.setItem('system-version', version?.result?.edition);
+          }
+        let result = getQueryString(window.location.hash)
+        if(result.client_id != undefined && result.response_type != undefined && result.redirect_uri != undefined && result.state != undefined){
+          apis.login.oauth(result).then((res) => {
+            if(res.status === 200){
+              window.location.href = res.result
+            }
+          })
+        }else{
+          const urlParams = new URL(window.location.href);
+          const params = getPageQuery();
+          let { redirect } = params as { redirect: string };
+          if (redirect) {
+            const redirectUrlParams = new URL(redirect);
+            if (redirectUrlParams.origin === urlParams.origin) {
+              redirect = redirect.substr(urlParams.origin.length);
+              if (redirect.match(/^\/.*#/)) {
+                redirect = redirect.substr(redirect.indexOf('#') + 1);
+              }
+            } else {
+              window.location.href = '/';
+              return;
+            }
+          }
+          // yield put(routerRedux.replace(redirect || '/'));
+          router.replace(redirect || '/');
+          // router.replace('/');
         }
-        // yield put(routerRedux.replace(redirect || '/'));
-        router.replace(redirect || '/');
-        // router.replace('/');
       } else (
         callback()
       )
