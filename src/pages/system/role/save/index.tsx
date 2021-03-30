4bb1b3d6f1c3b62adc6e920c27b5f1d7e6b76d70
sun-chaochao <2642441182@qq.com>
2021-03-30 10:24:46 +0800

    fix(角色编辑)：角色编辑时修改ID

--- src/pages/system/role/save/index.tsx
+++ src/pages/system/role/save/index.tsx
@@ -42,7 +42,7 @@ const Save: React.FC<Props> = props => {
           {getFieldDecorator('id', {
             rules: [{ required: true, message: '请输入角色标识' }],
             initialValue: data.id,
-          })(<Input placeholder="角色标识" />)}
+          })(<Input placeholder="角色标识" readOnly={!!data.id} />)}
         </Form.Item>
         <Form.Item label="角色名称">
           {getFieldDecorator('name', {
