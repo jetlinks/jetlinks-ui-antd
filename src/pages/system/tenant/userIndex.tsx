eb190a59be995d9f1409f9d3cea20683b10e9dd7
Lind <448929489@qq.com>
2021-04-08 16:08:01 +0800

    租户排序

--- src/pages/system/tenant/userIndex.tsx
+++ src/pages/system/tenant/userIndex.tsx
@@ -1,79 +1,84 @@
-import { PageHeaderWrapper } from "@ant-design/pro-layout"
-import React, { useEffect, useState } from "react"
-import { Avatar, Button, Card, List, message, Popconfirm, Radio, Spin, Tag } from "antd";
-import { PlusOutlined } from "@ant-design/icons";
-import moment from "moment";
-import { ListData } from "@/services/response";
-import { router } from "umi";
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
+import React, { useEffect, useState } from 'react';
+import { Avatar, Button, Card, List, message, Popconfirm, Radio, Spin, Tag } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+import moment from 'moment';
+import { ListData } from '@/services/response';
+import { router } from 'umi';
 import defaultImg from '@/assets/default.png';
-import { TenantItem } from "./data";
+import { TenantItem } from './data';
 import styles from './index.less';
-import Service from "./service";
-import Save from "./save";
-import { PaginationConfig } from "antd/lib/pagination";
-import encodeQueryParam from "@/utils/encodeParam";
+import Service from './service';
+import Save from './save';
+import { PaginationConfig } from 'antd/lib/pagination';
+import encodeQueryParam from '@/utils/encodeParam';
 
 const RadioButton = Radio.Button;
 const RadioGroup = Radio.Group;
 
-
-const ListContent = ({
-  data: { members, state, createTime },
-}: {
-  data: TenantItem;
-}) => (
-    <div className={styles.listContent}>
-      <div className={styles.listContentItem}>
-        <span>状态</span>
-        <p><Tag color={state.value === 'enabled' ? '#87d068' : '#F50'}>{state.text}</Tag></p>
-      </div>
-      <div className={styles.listContentItem}>
-        <span>成员数</span>
-        <p><Tag color="#87d068">{members}</Tag></p>
-      </div>
-      <div className={styles.listContentItem}>
-        <span>创建时间</span>
-        <p>{moment(createTime).format('YYYY-MM-DD HH:mm')}</p>
-      </div>
+const ListContent = ({ data: { members, state, createTime } }: { data: TenantItem }) => (
+  <div className={styles.listContent}>
+    <div className={styles.listContentItem}>
+      <span>状态</span>
+      <p>
+        <Tag color={state.value === 'enabled' ? '#87d068' : '#F50'}>{state.text}</Tag>
+      </p>
     </div>
-  );
-
+    <div className={styles.listContentItem}>
+      <span>成员数</span>
+      <p>
+        <Tag color="#87d068">{members}</Tag>
+      </p>
+    </div>
+    <div className={styles.listContentItem}>
+      <span>创建时间</span>
+      <p>{moment(createTime).format('YYYY-MM-DD HH:mm')}</p>
+    </div>
+  </div>
+);
 
 const UserTenant = () => {
-
   const service = new Service('tenant');
   const [loading, setLoading] = useState<boolean>(false);
   const [tloading, setTloading] = useState<boolean>(false);
   const [visible, setVisible] = useState<boolean>(false);
   const [current, setCurrent] = useState<Partial<TenantItem>>({});
   const [list, setList] = useState<ListData<TenantItem>>();
-  const [searchParam, setSearchParam] = useState({});
-
+  const [searchParam, setSearchParam] = useState({
+    sorts: {
+      field: 'id',
+      order: 'desc',
+    },
+  });
 
   const handleSearch = (params: any) => {
     setTloading(true);
     setSearchParam(params);
-    service.list(params).subscribe(data => {
+    service.list(encodeQueryParam(params)).subscribe(data => {
       setList(data);
       setTloading(false);
     });
   };
 
   useEffect(() => {
-    handleSearch({})
+    handleSearch(searchParam);
   }, []);
-
 
   const paginationProps: PaginationConfig = {
     showQuickJumper: true,
     pageSize: 5,
     total: list?.total || 0,
-    onChange: page => handleSearch({ pageIndex: page - 1, pageSize: 5 }),
+    onChange: (page, pageSize) => {
+      handleSearch({...searchParam, pageIndex: page - 1, pageSize: 5,  });
+    },
   };
 
   const extraContent = (
     <div className={styles.extraContent}>
-      <RadioGroup defaultValue="all" onChange={(e) => handleSearch(encodeQueryParam({ terms: { state: e.target.value } }))}>
+      <RadioGroup
+        defaultValue="all"
+        onChange={e => handleSearch(encodeQueryParam({ terms: { state: e.target.value } }))}
+      >
         <RadioButton value="">全部</RadioButton>
         <RadioButton value="enabled">正常</RadioButton>
         <RadioButton value="disabled">禁用</RadioButton>
@@ -85,7 +90,7 @@ const UserTenant = () => {
   const saveItem = (data: any) => {
     setLoading(true);
     service.create(data).subscribe(
-      (res) => {
+      res => {
         if (res) {
           message.success('保存成功');
           setLoading(false);
@@ -93,9 +98,11 @@ const UserTenant = () => {
           setCurrent({});
           handleSearch(searchParam);
         }
-      }, () => {
+      },
+      () => {
         message.error('保存失败');
-      });
+      },
+    );
   };
 
   const changeState = (item: any, state: string) => {
@@ -141,33 +148,34 @@ const UserTenant = () => {
               renderItem={(item: TenantItem) => (
                 <List.Item
                   actions={[
-                    <a onClick={e => {
-                      e.preventDefault();
-                      router.push(`/system/tenant/detail/${item.id}`)
-                    }}>查看</a>,
+                    <a
+                      onClick={e => {
+                        e.preventDefault();
+                        router.push(`/system/tenant/detail/${item.id}`);
+                      }}
+                    >
+                      查看
+                    </a>,
                     <>
-                      {
-                        item.state.value === 'enabled' ?
-                          (
-                            <Popconfirm title="确认禁用此租户？"
-                              onConfirm={() => {
-                                changeState(item, 'disabled');
-                              }}>
-                              <a key="edit">
-                                禁用
-                              </a>
-                            </Popconfirm>) :
-                          (
-                            <Popconfirm title="确认禁用此租户？"
-                              onConfirm={() => {
-                                changeState(item, 'enabled');
-                              }}>
-                              <a key="edit">
-                                启用
-                              </a>
-                            </Popconfirm>
-                          )
-                      }
+                      {item.state.value === 'enabled' ? (
+                        <Popconfirm
+                          title="确认禁用此租户？"
+                          onConfirm={() => {
+                            changeState(item, 'disabled');
+                          }}
+                        >
+                          <a key="edit">禁用</a>
+                        </Popconfirm>
+                      ) : (
+                        <Popconfirm
+                          title="确认禁用此租户？"
+                          onConfirm={() => {
+                            changeState(item, 'enabled');
+                          }}
+                        >
+                          <a key="edit">启用</a>
+                        </Popconfirm>
+                      )}
                     </>,
                   ]}
                 >
@@ -188,7 +196,7 @@ const UserTenant = () => {
         <Spin spinning={loading}>
           <Save
             close={() => {
-              setVisible(false)
+              setVisible(false);
             }}
             save={(item: any) => saveItem(item)}
             data={current}
@@ -196,6 +204,6 @@ const UserTenant = () => {
         </Spin>
       )}
     </PageHeaderWrapper>
-  )
+  );
 };
 export default UserTenant;
