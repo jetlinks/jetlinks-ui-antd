6867c8608f0a5b99c274bb94c5152826b8d0dc93
sun-chaochao <2642441182@qq.com>
2021-04-25 18:07:21 +0800

    OPC UA

---
+++ src/pages/network/opc-ua/save/bind-save.tsx
@@ -0,0 +1,183 @@
+import React, { useEffect, useState } from 'react';
+import Form from 'antd/es/form';
+import { FormComponentProps } from 'antd/lib/form';
+import { Input, message, Modal, Select, Spin } from 'antd';
+import { ConnectState } from '@/models/connect';
+import { connect } from 'dva';
+import apis from '@/services';
+import encodeQueryParam from '@/utils/encodeParam';
+
+interface Props extends FormComponentProps {
+    close: Function;
+    data: any;
+    save: Function;
+    opcId: string;
+}
+
+interface State {
+    productList: any[];
+    clusterList: any[];
+}
+
+const BindSave: React.FC<Props> = props => {
+    const initState: State = {
+        productList: [],
+        clusterList: [],
+    };
+    const [productList, setProductList] = useState(initState.productList);
+    // 消息协议
+    const [clusterList, setClusterList] = useState(initState.clusterList);
+    const [loading, setLoading] = useState<boolean>(true);
+    const {
+        form: { getFieldDecorator },
+        form,
+    } = props;
+    const submitData = () => {
+        form.validateFields((err, fileValue) => {
+            if (err) return;
+
+            const product: any =
+                productList.find(i => i.id === fileValue.productId) || {};
+
+            preservation({
+                ...fileValue,
+                productName: product.name
+            });
+        });
+    };
+
+    const preservation = (item: any) => {
+        if (props.data.id) {
+            apis.deviceInstance.saveOrUpdate(item)
+                .then((response: any) => {
+                    if (response.status === 200) {
+                        message.success('保存成功');
+                        props.save();
+                    }
+                })
+                .catch(() => {
+                });
+        } else {
+            let params = {
+                productId: item.productId,
+                productName: item.productName,
+                deviceId: item.id,
+                deviceName: item.name,
+                opcUaId: props.opcId,
+                serverId: item.serverId
+            }
+            apis.opcUa.saveDevice(params)
+                .then((response: any) => {
+                    if (response.status === 200) {
+                        message.success('创建成功');
+                        props.save();
+                    }
+                })
+                .catch(() => {
+                });
+        }
+    };
+
+    const queryClusterList = () => {
+        apis.opcUa.clusterList().then(res => {
+            if (res.status === 200) {
+                setClusterList(res.result);
+            }
+        })
+    };
+
+    useEffect(() => {
+        queryClusterList();
+        // 获取下拉框数据
+        apis.deviceProdcut
+            .queryNoPagin(encodeQueryParam({
+                paging: false,
+                terms: {
+                    messageProtocol: 'opc-ua'
+                }
+            }))
+            .then((response: any) => {
+                if (response.status === 200) {
+                    setLoading(false);
+                    setProductList(response.result);
+                }
+            })
+            .catch(() => {
+            });
+    }, []);
+
+    return (
+        <Modal
+            title={`${props.data.id ? '编辑' : '新建'}设备`}
+            visible
+            okText="确定"
+            cancelText="取消"
+            onOk={() => {
+                submitData();
+            }}
+            onCancel={() => props.close()}
+        >
+            <Spin spinning={loading}>
+                <Form labelCol={{ span: 4 }} wrapperCol={{ span: 20 }}>
+                    <Form.Item key="id" label="设备id">
+                        {getFieldDecorator('id', {
+                            rules: [
+                                { required: true, message: '请输入设备id' },
+                                { max: 64, message: '设备ID不超过64个字符' },
+                                { pattern: new RegExp(/^[0-9a-zA-Z_\-]+$/, "g"), message: '产品ID只能由数字、字母、下划线、中划线组成' }
+                            ],
+                            initialValue: props.data.id,
+                        })(<Input placeholder="请输入设备id" disabled={!!props.data.id} />)}
+                    </Form.Item>
+                    <Form.Item key="name" label="设备名称">
+                        {getFieldDecorator('name', {
+                            rules: [
+                                { required: true, message: '请输入设备名称' },
+                                { max: 200, message: '设备名称不超过200个字符' }
+                            ],
+                            initialValue: props.data.name,
+                        })(<Input placeholder="请输入设备名称" />)}
+                    </Form.Item>
+                    <Form.Item key="productId" label="产品">
+                        {getFieldDecorator('productId', {
+                            rules: [{ required: true }],
+                            initialValue: props.data.productId,
+                        })(
+                            <Select
+                                placeholder="请选择产品" disabled={!!props.data.id}>
+                                {(productList || []).map(item => (
+                                    <Select.Option
+                                        key={JSON.stringify({ productId: item.id, productName: item.name })}
+                                        value={item.id}
+                                    >
+                                        {item.name}
+                                    </Select.Option>
+                                ))}
+                            </Select>,
+                        )}
+                    </Form.Item>
+                    <Form.Item key="serverId" label="服务ID">
+                        {getFieldDecorator('serverId', {
+                            rules: [{ required: true }],
+                            initialValue: props.data.configuration?.opcClusterNodeId,
+                        })(
+                            <Select placeholder="请选择">
+                                {(clusterList || []).map(item => (
+                                    <Select.Option key={item.id} value={item.id}>
+                                        {item.id}
+                                    </Select.Option>
+                                ))}
+                            </Select>,
+                        )}
+                    </Form.Item>
+                </Form>
+            </Spin>
+        </Modal>
+    );
+};
+
+export default connect(({ deviceProduct, loading }: ConnectState) => ({
+    deviceProduct,
+    loading,
+}))(Form.create<Props>()(BindSave));
+
--- src/pages/network/opc-ua/service.tsx
+++ src/pages/network/opc-ua/service.tsx
@@ -1,5 +1,4 @@
 import request from '@/utils/request';
-import { OpcUaItem } from './data';
 
 export async function list(params?: any) {
     return request(`/jetlinks/opc/client/_query`, {
@@ -9,20 +8,20 @@ export async function list(params?: any) {
 }
 
 export async function listNoPaging(params?: any) {
-    return request(`/jetlinks/opc/client/_query/no-paging`, {
+    return request(`/jetlinks/opc/client/_query/no-paging?paging=false`, {
         method: 'GET',
         params: params,
     });
 }
 
-export async function save(params: OpcUaItem) {
+export async function save(params: any) {
     return request(`/jetlinks/opc/client`, {
         method: 'POST',
         data: params,
     });
 }
 
-export async function updata(params: OpcUaItem) {
+export async function updata(params: any) {
     return request(`/jetlinks/opc/client/${params.id}`, {
         method: 'PUT',
         data: params,
@@ -44,5 +43,70 @@ export async function start(id: string) {
 export async function stop(id: string) {
     return request(`/jetlinks/opc/client/${id}/_disable`, {
         method: 'POST',
+    });
+}
+
+export async function getDeviceBindList(params: any) {
+    return request(`/jetlinks/opc/device-bind/device-details/_query`, {
+        method: 'GET',
+        params
+    });
+}
+export async function getDeviceBindListNoPaging(params: any) {
+    return request(`/jetlinks/opc/device-bind/device-details/_query/no-paging?paging=false`, {
+        method: 'GET',
+        params
+    });
+}
+
+export async function getDevicePointList(params: any) {
+    return request(`/jetlinks/opc/device-bind/points/_query`, {
+        method: 'GET',
+        params
+    });
+}
+
+export async function removeBind(id: string) {
+    return request(`/jetlinks/device-instance/${id}`, {
+        method: 'DELETE',
+    });
+}
+
+export async function startBind(id: string) {
+    return request(`/jetlinks/opc/device-bind/${id}/_enable`, {
+        method: 'POST',
+    });
+}
+
+export async function stopBind(id: string) {
+    return request(`/jetlinks/opc/device-bind/${id}/_disable`, {
+        method: 'POST',
+    });
+}
+
+export async function savePoint(params: any) {
+    return request(`/jetlinks/opc/device-bind/points`, {
+        method: 'POST',
+        data: params
+    });
+}
+
+export async function clusterList() {
+    return request(`jetlinks/cluster/nodes`, {
+        method: 'GET'
+    });
+}
+
+export async function saveOrUpdate(params: any) {
+    return request(`/jetlinks/opc/device-bind/_create`, {
+        method: 'PATCH',
+        data: params,
+    });
+}
+
+export async function saveDevice(params: any) {
+    return request(`/jetlinks/opc/device-bind/_create`, {
+        method: 'POST',
+        data: params,
     });
 }
