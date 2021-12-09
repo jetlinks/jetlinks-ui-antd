2e175099a746339db324e058e345f46e80437bc6
Lind <448929489@qq.com>
2021-12-09 17:36:04 +0800

    build(ci): npm install --force

--- config/config.ts
+++ config/config.ts
@@ -73,7 +73,7 @@ export default defineConfig({
     },
   ],
   nodeModulesTransform: { type: 'none' },
-  mfsu: {},
+  // mfsu: {},
   webpack5: {},
   // exportStatic: {},
   chainWebpack(memo, { env, webpack, createCSSRule }) {
