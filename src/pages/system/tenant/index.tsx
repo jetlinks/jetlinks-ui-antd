c3923009b7396a157e04d1f2a93d2cfd39369694
Lind <448929489@qq.com>
2022-01-24 16:49:23 +0800

    fix(tenant): fix length

--- src/pages/system/tenant/index.tsx
+++ src/pages/system/tenant/index.tsx
@@ -1,37 +1,37 @@
-import React from "react";
-import UserTenant from "@/pages/system/tenant/userIndex";
-import Detail from "@/pages/system/tenant/detail";
-import Analysis from "@/pages/analysis";
+import React from 'react';
+import UserTenant from '@/pages/system/tenant/userIndex';
+import Detail from '@/pages/system/tenant/detail';
+import Analysis from '@/pages/analysis';
 
-interface Props {
-}
+interface Props {}
 
 class Tenant extends React.Component<Props> {
   render() {
     const render = () => {
-
       const tenant = localStorage.getItem('hsweb-autz');
       try {
         let tenantsList: any[] = JSON.parse(tenant as string).user.tenants;
-        if (tenantsList.length > 0) {
+        if (tenantsList?.length > 0) {
           return tenantsList.map((item: any) => {
             if (item.mainTenant && item.adminMember) {
-              return <Detail location={
-                {
-                  pathname: `/system/tenant/detail/${item.tenantId}`,
-                  search: '',
-                  hash: "",
-                  query: {},
-                  state: undefined,
-                }
-              } />
+              return (
+                <Detail
+                  location={{
+                    pathname: `/system/tenant/detail/${item.tenantId}`,
+                    search: '',
+                    hash: '',
+                    query: {},
+                    state: undefined,
+                  }}
+                />
+              );
             } else {
               // return <Analysis />
               return null;
             }
-          })
+          });
         } else {
-          return <UserTenant />
+          return <UserTenant />;
         }
       } catch (error) {
         return false;
