e2584a4a91b3b8b8f5499616a125d26a26f29a41
sun-chaochao <2642441182@qq.com>
2022-03-24 11:39:37 +0800

    fix: 3.24修改bug

--- src/pages/link/AccessConfig/Detail/index.tsx
+++ src/pages/link/AccessConfig/Detail/index.tsx
@@ -1,30 +1,17 @@
 import { PageContainer } from '@ant-design/pro-layout';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useLocation } from 'umi';
 import Access from './Access';
 import Provider from './Provider';
-import { service } from '@/pages/link/AccessConfig';
 
 type LocationType = {
   id?: string;
 };
 
 const Detail = () => {
-  const [visible, setVisible] = useState<boolean>(true);
-  const [data, setData] = useState<any>({});
-  const [config, setConfig] = useState<any>({});
-
   const location = useLocation<LocationType>();
-
-  useEffect(() => {
-    const params = new URLSearchParams(location.search);
-    if (params.get('id')) {
-      service.detail(params.get('id') || '').then((resp) => {
-        setConfig(resp.result);
-        setVisible(false);
-      });
-    }
-  }, []);
+  const [visible, setVisible] = useState<boolean>(!new URLSearchParams(location.search).get('id'));
+  const [data, setData] = useState<any>({});
 
   return (
     <PageContainer>
@@ -38,7 +25,6 @@ const Detail = () => {
       ) : (
         <Access
           data={data}
-          access={config}
           change={() => {
             setVisible(true);
           }}
