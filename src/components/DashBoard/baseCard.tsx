f73665754c0ef240b61182188821ae1d0a08f18e
xieyonghong <18010623010@163.com>
2022-05-30 16:56:58 +0800

    feat(视频设备仪表盘): 1.添加公用组件；2.添加仪表盘页面；

---
+++ src/components/DashBoard/baseCard.tsx
@@ -0,0 +1,30 @@
+import Header from './header';
+import type { HeaderProps } from './header';
+import Echarts from './echarts';
+import type { EchartsProps } from './echarts';
+import Style from './index.less';
+import classNames from 'classnames';
+
+interface BaseCardProps extends HeaderProps, EchartsProps {
+  height: number;
+  classNames?: string;
+}
+
+export default (props: BaseCardProps) => {
+  return (
+    <div
+      className={classNames(Style['dash-board-echarts'], props.classNames)}
+      style={{
+        height: props.height || 200,
+      }}
+    >
+      <Header
+        title={props.title}
+        onParamsChange={props.onParamsChange}
+        extraParams={props.extraParams}
+        initialValues={props.initialValues}
+      />
+      <Echarts options={props.options} />
+    </div>
+  );
+};
---
+++ src/components/DashBoard/index.tsx
@@ -0,0 +1,6 @@
+import BaseCard from './baseCard';
+
+export { default as DashBoardEcharts } from './echarts';
+export { default as DashBoardHeader } from './header';
+
+export default BaseCard;
