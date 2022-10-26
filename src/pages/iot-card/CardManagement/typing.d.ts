4a7a7ea9f5dfa22e2f53d85eadf298599c2734f3
xieyonghong <18010623010@163.com>
2022-10-26 09:59:43 +0800

    feat: 添加物联网卡管理Service文件

---
+++ src/pages/iot-card/CardManagement/typing.d.ts
@@ -0,0 +1,15 @@
+export type CardManagement = {
+  id: string;
+  name: string;
+  iccId: string;
+  deviceId: string;
+  platformConfigId: string;
+  operatorName: string;
+  cardType: string;
+  totalFlow: string;
+  usedFlow: string;
+  residualFlow: string;
+  activationDate: string;
+  updateTime: string;
+  cardStateType: string;
+};
