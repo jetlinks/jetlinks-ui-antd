2424405333b50ea37f2dd0ca8ddb5f4e81d4daf3
haotian.liu <sky_lht9527@qq.com>
2023-02-13 22:21:22 +0800

    💥 feat(compiler): 修改ci路径

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 API_BASE_PATH=$API_BASE_PATH;
 NAMESERVERS=$(cat /etc/resolv.conf | grep "nameserver" | awk '{print $2}' | tr '\n' ' ')
 if [ -z "$API_BASE_PATH" ]; then
-    API_BASE_PATH="http://10.0.0.151:10005/";
+    API_BASE_PATH="http://10.0.0.151:10005/api/";
 fi
 
 apiUrl="proxy_pass  $API_BASE_PATH\$1;"
