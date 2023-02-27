c6a503785c9f94107294d02e772f301086ebdd85
wzyyy <1175160654@qq.com>
2023-02-27 18:13:36 +0800

    fix: docker配置

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker build -t registry.cn-shenzhen.aliyuncs.com/jetlinks/jetlinks-ui-pro:2.0.0-SNAPSHOT .
-docker push registry.cn-shenzhen.aliyuncs.com/jetlinks/jetlinks-ui-pro:2.0.0-SNAPSHOT
+docker build -t registry.cn-shenzhen.aliyuncs.com/jetlinks/jetlinks-ui-pro:2.0.0 .
+docker push registry.cn-shenzhen.aliyuncs.com/jetlinks/jetlinks-ui-pro:2.0.0
